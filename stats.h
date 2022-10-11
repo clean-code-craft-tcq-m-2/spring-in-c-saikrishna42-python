18e966593ad77cce25499b0730c3550cbe2967a0
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-10-11 12:57:25 +0200

    Update stats.h
    defining the function

--- stats.h
+++ stats.h
@@ -11,3 +11,7 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+
+
+void emailAlerter(void);
+void ledAlerter(void);
