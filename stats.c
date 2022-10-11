95706c32306efa544aa0c61985a39d2eedaf2067
saikrishna42-python <74580746+saikrishna42-python@users.noreply.github.com>
2022-10-11 11:47:44 +0200

    Update stats.c
    caluclating the min,max and avg

--- stats.c
+++ stats.c
@@ -7,7 +7,31 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.max = 0;
     float sum=0;
     float max=0,min=0;
-    
+    min=*numberset;
+    max=*numberset;
+    if(setlength == 0)
+    {
+        s.avg=NAN;
+        s.min=NAN;
+        s.max=NAN;
+    }
+    else
+    {
+        
+        for(i=0;i<=setlength-1;i++)
+        {
+            sum+=*numberset;
+            if(min<*numberset)
+                min=*numberset;
+            if(max>*numberset)
+                max=*numberset;
+            
+        }
+        numberset++;
+        s.avg=sum/setlength;
+        s.min=min;
+        s.max=max;
+    }
     
     return s;
 }
